199b3e861df0e5da7f16a2d9bb31f08891f5d366
azarins1 <az61768@djusdstudents.org>
2019-09-12 20:12:53 -0700

    Completed the Chapter 1 assignment: ch_1_assign_Arthur_Zarins_.py

--- ch_1_Arthur_Zarins.py
+++ ch_1_Arthur_Zarins.py
@@ -1,2 +1,3 @@
-print('Hello, world! ipjsed42d')
+print('Hello, world!')
 print("To run this in VS code, use Ctrl Fn F5")
+print("1678 PARENT INFO NIGHT this saturday! 4:30-6:30")
---
+++ ch_1_assign_Arthur_Zarins_.py
@@ -0,0 +1,39 @@
+#Reminder for self: to run python programs in Visual Studio code, use Cntrl + Fn + F5
+teams = {
+	'1678': {'Last Year': 2019,
+            'Location': "Davis CA",
+            'Rookie Year': 2005, 
+            'Events': 'Sac Regional, Central Valley Regional'
+            },
+	'2551': {'Last Year': 2019, 
+            'Location': "Novato CA", 
+            'Rookie Year': 2008, 'Events': 
+            'Sac Regional, SF Regional, Galileo divison'},
+    '3421': {'Last Year': 2013, 
+            'Location': "Marysville MI", 
+            'Rookie Year': 2010, 
+            'Events': 'MY Regional'},
+    '2942': {'Last Year': 2018, 
+            'Location': "Seattle, WA", 
+            'Rookie Year': 2009, 
+            'Events': ''}
+}
+#reference = "is"
+def teamStats(teamName, attribute):
+    #New variable refernce will handle grammer related to pluralities
+    reference = "is"
+    stringTest = teams[teamName][attribute].split()
+    attributeDisplay = attribute
+    if attribute == "Events" and len(stringTest) > 2:
+        reference = "are"
+    else:
+        if attribute == "Events":
+            attributeDisplay = "only Event"
+        reference = "is"
+    print("Team " + teamName + "'s " + attributeDisplay + " " + reference + " " + teams[teamName][attribute])
+
+#The big test:
+teamStats('1678', 'Location')
+teamStats('1678', 'Events')
+#1 event: the grammer works!
+teamStats('3421', 'Events')
