d6bf0d6cc9797ad1791e924d6c9e7acce790b0e3
azarins1 <az61768@djusdstudents.org>
2019-09-12 17:19:29 -0700

    9/12/2019 @ 5:19 PM

--- ch_1_Arthur_Zarins.py
+++ ch_1_Arthur_Zarins.py
@@ -1,1 +1,2 @@
 print('Hello, world! ipjsed42d')
+print("To run this in VS code, use Ctrl Fn F5")
---
+++ ch_1_Arthur_Zarins_hw.py
@@ -0,0 +1,5 @@
+teams = {
+	'1678': {'Weight':200, 'Wheels': 4, 'Speed': 9},
+	'2583': {'Weight': 300, 'Wheels': '325', 'Speed': 3}
+}
+print(teams['1678']['Weight'])
