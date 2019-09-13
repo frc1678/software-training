60e3d9e0037285620d9cfaecf96f84d230d0ccd8
LH <logannhaug@gmail.com>
2019-09-12 20:37:54 -0700

    Changed key type to int, fixe some formatting issues within the dictionary.

--- ch_1_assign_logan_haug.py
+++ ch_1_assign_logan_haug.py
@@ -1,6 +1,6 @@
 #This dictionary stores all of the information related to the teams,
 #The Key of the main Dict is the team number, and its dictionary are its attributes
-teamInfo = {
+team_info = {
     "1678": {
         "Location" : "Davis, California, USA",
         "Rookie Year" : 2005,
@@ -8,13 +8,13 @@ teamInfo = {
         "Competitions" : {
             "Central Valley Regional": "Fresno, CA, USA",
             "Sacramento Regional":"Davis, CA, USA",
-            "Aerospace Valley Regional":"Lancaster, CA, USA",
+            "Aerospace Valley Regional": "Lancaster, CA, USA",
             "2019 Championships":"Houston, TX, USA",
             "RCC Qianjiang International Robotics Invitational":"Hangzhou, Zhejiang, China"
         },
         "Awards in 2019 Season": ["Regional Chairman's Award", "Regional Winners","FIRST Dean's List Finalist Award (Katie Stachowicz)","Regional Winners","Industrial Design Award sponsored by General Motors","Regional Winners","Excellence in Engineering Award sponsored by Delphi","Championship Subdivision Winner","Entrepreneurship Award sponsored by Kleiner Perkins Caufield and Byers"]
     },
-    "1": {
+    1: {
         "Location" : "Pontiac, Michigan, USA",
         "Rookie Year" : 1997,
         "Competed in 2019 Season" : True,
@@ -24,7 +24,7 @@ teamInfo = {
         },
         "Awards in 2019 Season": ["Imagery Award in honor of Jack Kamen"]
     },
-    "4": {
+    4: {
         "Location" : "Van Nuys, California, USA",
         "Rookie Year" : 1997,
         "Competed in 2019 Season" : True,
@@ -36,14 +36,14 @@ teamInfo = {
         },
         "Awards in 2019 Season": ["Quality Award sponsored by Motorola Solutions Foundation"]
     },
-    "6": {
+    6: {
         "Location" : "Plymouth, MN, USA",
         "Rookie Year" : 1994,
         "Competed in 2019 Season" : False,
         "Competitions" : "No Competitions in the 2019 season",
         "Awards in 2019 Season": ["No awards in the 2019 season"]
     },
-    "5": {
+    5: {
         "Location" : "Melvindale, MI, USA",
         "Rookie Year" : 1998,
         "Competed in 2019 Season" : True,
@@ -54,6 +54,6 @@ teamInfo = {
     },
 }
 #Takes user team number, attribute, and prints it
-uteam = input("enter an frc team: ")
-uattribute = input("enter an attribute: ")
-print(teamInfo[uteam][uattribute])
+user_team = int(input("enter an frc team: "))
+user_attribute = input("enter an attribute: ")
+print(teamInfo[user_team][user_attribute])
