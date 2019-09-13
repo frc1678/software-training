5462d68a7bc6ae217106ffcb9e3e8a6a52f24fef
Jackson Anglin-Hill <ja61224@djusdstudents.org>
2019-09-12 20:46:36 -0700

    Did homework/assignment 1

---
+++ Homework_1.py
@@ -0,0 +1,6 @@
+team = {
+	1678: {"weight": 123, "color": "blue", "wheels": "green"}
+	123: {"weight": 145, "color": "red", "wheels": "yellow"}
+	9432: {"weight": 1678, "color": "green", "wheels": "blue"}
+}
+requested_team_number = input("Team Number: ")
