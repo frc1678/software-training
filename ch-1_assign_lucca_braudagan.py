b18d7ea629629d2ed4f61fd76fb5ce840c1b2752
luccambraudagan <lbraudagan@gmail.com>
2019-09-12 20:46:54 -0700

    Add program to file

---
+++ ch-1_assign_lucca_braudagan.py
@@ -0,0 +1,75 @@
+teams={
+	"1678_citrus_circuits":{
+		"location":"Davis, CA", 
+		"rookie_year":2005, 
+		"competed_in_2019_seaseon":True, 
+		"2019_competition_names":[
+			"Central_Valley_Regional",
+			"Sacramento_Regional",
+			"Aerospace_Valley_Regional",
+			"Carver_Division",
+			"Einstein_Field",
+			"RCC_Qianjiang_International_Robotics_Invitational",
+			"Chezy_Champs"],
+		"2019_competition_locations":[
+			"Fresno_CA",
+			"Davis_CA",
+			"Lancaster_CA",
+			"Houston_TX",
+			"Houston_TX_Again"
+			"Hangzhou_Zhejiang_China",
+			"San_Jose"],
+		"2019_season_awards":[
+			"Chairman's_Award_At_Central_Valley_Regional",
+			"Winner_At_Central_Valley_Regional",
+			"Winner_At_Sacramento_Regional",
+			"Winner_At_Aerospace_Valley_Regional",
+			"Winner_At_Carver_Divison"]
+	},
+	"4322_clockwork_oranges":{
+		"location":"Orange, CA",
+		"rookie_year":2012,
+		"competed_in_2019_seaseon":True,
+		"2019_competition_names":[
+			"San_Diego_Regional_Presented_By_Qualcomm",
+			"Las_Vegas_Regional",
+			"Einstein_Field",
+			"Battleship_Blast_Monday",
+			"Beach_Blitz"],
+		"2019_competition_locations":[
+			"Del_Mar_CA",
+			"Las_Vegas_NV",
+			"Houston_TX",
+			"San_Pedro_CA",
+			"Huntington_Beach_CA"],
+		"2019_season_awards":"None"
+	},
+	"5458_digital_minds":{
+		"location":"Woodland, CA",
+		"rookie_year":2015,
+		"competed_in_2019_seaseon":True,
+		"2019_competition_names":[
+			"Central_Valley_Regional",
+			"Sacramento_Regional"],
+		"2019_competition_locations":[
+			"Fresno_CA",
+			"Davis_CA"],
+		"2019_season_awards":"None"
+	}
+	"1_the_juggernauts":{
+		"location":"Pontiac, MI",
+		"rookie_year":1997,
+		"competed_in_2019_seaseon":True,
+		"2019_competition_names":[
+			"FIM_District_Center_Line_Event",
+			"FIM_District_Troy_Event"]
+		"2019_competition_locations":[
+			"Center_Line_MI",
+			"Troy_MI"]
+		"2019_season_awards":"None"
+	}
+	"7229"
+}
+team_number=input("Team:")
+team_information=input("Category:")
+print(teams[team_number][team_information])
