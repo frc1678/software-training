a322a7544c4885f925c563b62c79365d580aca15
kevinrockwell <kevinnrockwell@gmail.com>
2019-09-15 13:09:01 -0700

    Add Chapter 1 Assignment
    - Create teams dictionary with data for 5 teams

---
+++ ch_1_assign_kevin_rockwell.py
@@ -0,0 +1,65 @@
+teams = { #Stores all the teams with string form of team number as key
+    "1": {
+        "lcoation": "Pontiac, Michigan, USA",
+        "rookie_year": 1997,
+        "competed_2019": True,
+        "2019_comps": {
+            "FIM District Center Line Event 2019": "Center Line, MI, USA",
+            "FIM District Troy Event 2019": "Troy, MI, USA",
+        },
+        "2019_awards": [
+            "Imagery Award in honor of Jack Kamen",
+        ],
+    },
+    "16": {
+        "lcoation": "Mountain Home, Arkansas, USA",
+        "rookie_year": 1996,
+        "competed_2019": True,
+        "2019_comps": {
+            "Midwest Regional": "Chicago, IL, USA",
+            "Rocket City Regional 2019": "Huntsville, AL, USA",
+            "Darwin Division 2019": "Detriot, MI, USA",
+        },
+        "2019_awards": [
+            "Industrial Design Award sponsored by General Motors",
+            "Regional Finalists",
+            "Excellence in Engineering Award sponsored by Delphi",
+        ],
+    },
+    "253": {
+            "location": "Millbrae, California, USA",.
+            "rookie_year": 1999,
+            "competed_2019": True,
+            "2019_comps": {
+                "San Fransisco Regional": "San Fransisco, CA, USA",
+                "Monterey Bay Regional": "Seaside, CA, USA",
+                "Newton Divison": "Houston, TX, USA"
+            },
+            "2019_awards": [
+                "Team Spirit Award sponsored by FCA Foundation",
+            ],
+    },
+	"342": {
+        "location": "North Charleston, South Carolina, USA",
+            "rookie_year": 2000,
+            "competed_2019": True,
+            "2019_comps": {
+                "Palmetto Regional 2019": "Myrtle Beach, SC, USA",
+                "Rocket City Regional 2019": "Huntsville, AL, USA",
+            },
+            "2019_awards": [],
+    },
+    "554": {
+        "location": "Ft. Thomas, Kentucky, USA",
+            "rookie_year": 2001,
+            "competed_2019": True,
+            "2019_comps": {
+                "Miami Valley Regional 2019": "Fairborn, OH, USA",
+            },
+            "2019_awards": [],
+    }
+
+}
+
+
+
