5c8cbcf0570cdaf90ef4b37309a391126944c216
luccambraudagan <lbraudagan@gmail.com>
2019-09-15 19:44:57 -0700

    Add program to file

--- ch-1_assign_lucca_braudagan.py
+++ ch-1_assign_lucca_braudagan.py
@@ -1,5 +1,6 @@
 teams={
-	"1678_citrus_circuits":{
+	"1678":{
+		"team_name":"Citrus Circuits",
 		"location":"Davis, CA", 
 		"rookie_year":2005, 
 		"competed_in_2019_seaseon":True, 
@@ -20,13 +21,18 @@ teams={
 			"Hangzhou_Zhejiang_China",
 			"San_Jose"],
 		"2019_season_awards":[
-			"Chairman's_Award_At_Central_Valley_Regional",
-			"Winner_At_Central_Valley_Regional",
-			"Winner_At_Sacramento_Regional",
-			"Winner_At_Aerospace_Valley_Regional",
-			"Winner_At_Carver_Divison"]
+			"Regional_Chairman's_Award_At_Central_Valley_Regional",
+			"Regional_Winners_At_Central_Valley_Regional",
+			"FIRST_Dean's_List_Finalist_Award",
+			"Regional_Winners_At_Sacramento_Regional",
+			"Industrial_Design_Award_Sponsored_By_General_Motors",
+			"Regional_Winners_At_Aerospace_Valley_Regional",
+			"Excellence_In_Engineering_Award_Sponsered_By_Delphi",
+			"Championship_Subdivision_Winner_In_Carver_Divison",
+			"Entrepreneurship_Award_Sponsored_By_Kleiner_Perkins_Caufield_And_Byers"]
 	},
-	"4322_clockwork_oranges":{
+	"4322":{
+		"team_name":"Clockwork Oranges",
 		"location":"Orange, CA",
 		"rookie_year":2012,
 		"competed_in_2019_seaseon":True,
@@ -42,9 +48,12 @@ teams={
 			"Houston_TX",
 			"San_Pedro_CA",
 			"Huntington_Beach_CA"],
-		"2019_season_awards":"None"
+		"2019_season_awards":[
+			"FIRST_Dean's_List_Finalist_Award",
+			"FIRST_Dean's_List_Award"]
 	},
-	"5458_digital_minds":{
+	"5458":{
+		"team_name":"Digital Minds",
 		"location":"Woodland, CA",
 		"rookie_year":2015,
 		"competed_in_2019_seaseon":True,
@@ -55,20 +64,29 @@ teams={
 			"Fresno_CA",
 			"Davis_CA"],
 		"2019_season_awards":"None"
-	}
-	"1_the_juggernauts":{
+	},
+	"1":{
+		"team_name":"The Juggernauts",
 		"location":"Pontiac, MI",
 		"rookie_year":1997,
 		"competed_in_2019_seaseon":True,
 		"2019_competition_names":[
 			"FIM_District_Center_Line_Event",
-			"FIM_District_Troy_Event"]
+			"FIM_District_Troy_Event"],
 		"2019_competition_locations":[
 			"Center_Line_MI",
-			"Troy_MI"]
+			"Troy_MI"],
+		"2019_season_awards":"Imagery_Award_In_Honor_Of_Jack_Kamen"
+	},
+	"7229":{
+		"team_name":"Electronic Eagles",
+		"location":"Sacramento, CA",
+		"rookie_year":2018,
+		"competed_in_2019_seaseon":True,
+		"2019_competition_names":"Sacramento_Regional",
+		"2019_competition_locations":"Davis_CA",
 		"2019_season_awards":"None"
 	}
-	"7229"
 }
 team_number=input("Team:")
 team_information=input("Category:")
