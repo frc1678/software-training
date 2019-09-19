457a616eeedb0cfe7bb8de317b72d6a77df68c46
jonathanhoNumberXIII <jonathanbho@gmail.com>
2019-09-18 19:55:56 -0700

    I placed the data of each robotics team into nests of dictionaries and printed them out through the input variable.

--- ch_1_assign_Jonathan_Ho.py
+++ ch_1_assign_Jonathan_Ho.py
@@ -8,28 +8,114 @@ Robot_comp_locate
 Robot_comp_award
 """
 
-team_1678 = {'Location': {'Show location': 'Davis, CA, USA'}, 'Rookie': {'Rookie Year': '2005'}, 'Season 2019': {'Participate?': 'True'},
-	'Competition Names': ['Central Valley Regional', 'Sacramento Regional', 'Aerospace Valley Regional', 'Carver Division', 'Einstein Field', 'RCC Qianjiang International Robotics Invitational'], 
-	'Competition Location': ['Fresno, CA, USA', 'Davis, CA, USA', 'Lancaster, CA, USA', 'Houston, TX, USA', 'Houston TX, USA', 'Hangzhou, Zhejiang, China'],
-	'Awards': ['Regional Chairman`s Award and Regional Winners', 'FIRST Dean`s List Finalist Award (Katie Stachowicz), Regional Winners, and Industrial Design Award sponsored by General Motors',
-	'Regional Winners and Excellence in Engineering Award sponsored by Delphi', 'Championship Subdivision Winner and Entrepreneurship Award sponsored by Kleiner Perkins Caufield and Byers',]}
+team_1678 = {
+    'Location': {
+        'Show location': 'Davis, CA, USA'
+    }, 'Rookie': {
+       'Rookie Year': '2005'
+    }, 'Season 2019': {
+    	'Participate?': 'True'
+    },
+	'Competition Names': [
+		'Central Valley Regional',
+		'Sacramento Regional',
+		'Aerospace Valley Regional', 
+		'Carver Division', 
+		'Einstein Field', 
+		'RCC Qianjiang International Robotics Invitational'
+	], 
+	'Competition Location': [
+		'Fresno, CA, USA', 
+		'Davis, CA, USA', 
+		'Lancaster, CA, USA', 
+		'Houston, TX, USA', 
+		'Houston TX, USA', 
+		'Hangzhou, Zhejiang, China'
+	],
+	'Awards': [
+		'Regional Chairman`s Award and Regional Winners', 
+		'FIRST Dean`s List Finalist Award (Katie Stachowicz), Regional Winners, and Industrial Design Award sponsored by General Motors',
+		'Regional Winners and Excellence in Engineering Award sponsored by Delphi', 'Championship Subdivision Winner and Entrepreneurship Award sponsored by Kleiner Perkins Caufield and Byers'
+	,]
+	}
 
-team_1111 = {'Location': {'Show location': 'Edgewater, Maryland, USA'}, 'Rookie': {'Rookie Year': '2003'}, 'Season 2019': {'Participate?': 'True'},
-	'Competition Names': ['CHS District Bethesda MD Event sponsored by Bechtel', 'CHS District Owings Mills MD Event sponsored by Leidos', 'FIRST Chesapeake District Championship'],
-	'Competition Location': ['Bethesda, MD 20817, USA', 'Owings Mills, MD 21117, USA', 'Fairfax, VA 22030, USA'],
-	'Awards': ['Autonomous Award sponsored by Ford', 'District Chairman`s Award', 'Team Spirit Award sponsored by FCA Foundation']}
+team_1111 = {
+	'Location': {
+		'Show location': 'Edgewater, Maryland, USA'
+	}, 'Rookie': {
+		'Rookie Year': '2003'
+	}, 'Season 2019': {
+		'Participate?': 'True'
+	},
+	'Competition Names': [
+		'CHS District Bethesda MD Event sponsored by Bechtel',
+		'CHS District Owings Mills MD Event sponsored by Leidos', 
+		'FIRST Chesapeake District Championship'
+	],
+	'Competition Location': [
+		'Bethesda, MD 20817, USA', 
+		'Owings Mills, MD 21117, USA', 
+		'Fairfax, VA 22030, USA'
+	],
+	'Awards': [
+		'Autonomous Award sponsored by Ford', 
+		'District Chairman`s Award', 
+		'Team Spirit Award sponsored by FCA Foundation'
+	]
+	}
 
-team_2222 = {'Location': {'Show location': 'Tacoma, WA, USA'}, 'Rookie': {'Rookie Year': '2007'}, 'Season 2019': {'Participate?': 'False'},
-	'Competition Names': {'Competition': 'Pacific Northwest Regional 2007'}, 'Competition Location': {'Location of Competition': 'Pioneer Courthouse Square in Portland, OR, USA'},
-	'Awards': {'Award': 'None'}}
+team_2222 = {
+	'Location': {
+		'Show location': 'Tacoma, WA, USA'
+	}, 'Rookie': {
+		'Rookie Year': '2007'
+	}, 'Season 2019': {
+		'Participate?': 'False'
+	},
+	'Competition Names': {
+		'Competition': 'Pacific Northwest Regional 2007'
+	}, 'Competition Location': {
+		'Location of Competition': 'Pioneer Courthouse Square in Portland, OR, USA'
+	},
+	'Awards': {
+		'Award': 'None'
+	}
+	}
 
-team_3333 = {'Location': {'Show location': 'Julesburg, CO, USA'}, 'Rookie': {'Rookie Year': '2010'}, 'Season_2019': {'Participate?': 'False'},
-	'Competition Names': {'Competition': 'Colorado Regional 2010'}, 'Competition Location': {'Location of Competition': 'Ritchie Center in Denver, CO, USA'},
-	'Awards': {'Award': 'Judges Award'}}
+team_3333 = {
+	'Location': {
+		'Show location': 'Julesburg, CO, USA'
+	}, 'Rookie': {
+		'Rookie Year': '2010'
+	}, 'Season_2019': {
+		'Participate?': 'False'
+	},
+	'Competition Names': {
+		'Competition': 'Colorado Regional 2010'
+	}, 'Competition Location': {
+		'Location of Competition': 'Ritchie Center in Denver, CO, USA'
+	}, 'Awards': {
+	'Award': 'Judges Award'
+	}
+	}
 
-team_5555 = {'Location': {'Show Location': 'Warren, Michigan'}, 'Rookie': {'Rookie Year': '2015'}, 'Season 2019': {'Participate?': 'True'},
-	'Competition Names': ['FIM District Center Line Event','FIM District Marysville Event'], 'Competition Location': ['Center Line, MI, USA', 'Marysville, MI, USA'], 
-	'Awards': {'Award': 'District Event Winner'}}
+team_5555 = {
+	'Location': {
+		'Show Location': 'Warren, Michigan'
+	}, 'Rookie': {
+		'Rookie Year': '2015'
+	}, 'Season 2019': {
+		'Participate?': 'True'
+	}, 'Competition Names': [
+		'FIM District Center Line Event',
+		'FIM District Marysville Event'
+	], 'Competition Location': [
+		'Center Line, MI, USA',
+		'Marysville, MI, USA'
+	], 'Awards': {
+		'Award': 'District Event Winner'
+	}
+	}
 
 
 print(Robot_Team)
