b628f4b7a7ba3921d767406fbedb7fe1aa42013b
Olivia Sagayaradj <olivia@Olivias-MacBook-Air.local>
2019-09-18 18:57:36 -0700

    write training 1

--- chapter_1_lesson_olivia_sagayaradj.py
+++ chapter_1_lesson_olivia_sagayaradj.py
@@ -1,1 +1,51 @@
-print("hello world")
+team_1111 = {'location':
+	Edgewater Maryland USA,
+	'rookie year':
+	2003,
+	'competed in 2019':
+	True,
+	'competitions in 2019':
+	CHS District Bethesda MD Event sponsored by Bechtel  CHS District Ownings Mills MD Event sponsored by Leidos  FIRST Chesapeake District Champion,
+	'location of 2019 competitions':
+	Bethesda MD 20817 USA  Owings Mills MD 21117 USA  Fairfax VA 22030 USA,
+	'2019 season awards':
+	Autonomous Award sponsored by Ford District Chairmans Award Team Spirit Award sponsored by FCA Foundation}
+team_2222 = {'location':
+	Tacoma WA USA,
+	'rookie year':
+	2007,
+	'competed in 2019':
+	False}
+team_3333 = {'location':
+	Julesburg CO USA,
+	'rookie year':
+	2010,
+	'competed in 2019':
+	False}
+team_1678 = {'location':
+	Davis CA USA,
+	'rookie year':
+	2005,
+	'competed in 2019':
+	True,
+	'competitions in 2019':
+	Central Valley Regional  Sacramento Regional  Aerospace Valley Regional  Carver Division  Einstein Field Houston  RCC Qianjiang International Robotics Invitational  Chezy Champs,
+	'location of 2019 competitions':
+	Fresno CA USA  Davis CA USA  Lancaster CA USA  Houston TX USA  Houston TX USA  Hangzhou Zhejiang China  San Jose CA USA,
+	'2019 season awards':
+	Regional Chairmans Award  Regional Winners  
+	FIRST Deans List Finalist Award Katie Stachowicz  Regional Winners  Industrial Design Award sponsored by General Motors
+	Regional Winners  Excellence in Engineering Award sponsored by Delphi  
+	Championship Subdivision Winner  Entrepreneurship Award sponsored by Klieiner Perkins Caufield and Byers}
+team_5555 = {'location': 
+	Warren Michigan USA
+	'rookie year'
+	2015,
+	'competed in 2019':
+	True,
+	'competitions in 2019':
+	FIM District Center Line Event  FIM District Marysville Event,
+	'location of 2019 competitions':
+	Center Line MI USA  Marysville MI USA,
+	'2019 season awards'
+	District Event Winner}
