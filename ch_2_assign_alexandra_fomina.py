b6c68fe94f6421fdf73a0ad03a2376eeaacabf0b
Alexandra <alexandra.a.fomina@gmail.com>
2019-09-19 20:32:54 -0700

    Chapter 2 Assign.

---
+++ ch_2_assign_alexandra_fomina.py
@@ -0,0 +1,16 @@
+teams = {}
+while True:
+    command = input(""" Type add to add a team, 
+    	Type remove to remove a team, 
+    	Type search to search for a team, 
+    	Type list all teams to display all teams, 
+    	Type quit to exit
+    	""")
+    if command == "add" :
+        add = input("What is the team number?")
+        teams[add] = {}
+
+    if command == quit
+        break
+#print (""" Type 'add' to add a team
+#Type 'information' to see team's information)
