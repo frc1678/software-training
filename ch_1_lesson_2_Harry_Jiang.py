e91e9a5218c39caef17946ef1bb10faf6a693eb6
Harry Jiang <harryzjiang@gmail.com>
2019-09-25 20:01:13 -0700

    changed formatting and added user input

--- ch_1_lesson_2_Harry_Jiang.py
+++ ch_1_lesson_2_Harry_Jiang.py
@@ -1,45 +1,50 @@
 team_dict = {
 
-	"1_dict" :
-	    {'1_location' : "Pontact, Michigan, USA",
-	"1_rookie_year" : 1997 ,
-	"1_2019_competition" : True,
-	"1_2019_competition_names" : ['FIM Distric Center Line Event', 'FIM District Troy Event'],
-	"1_2019_competition_location" : ['Center Line, MI,USA', 'Troy, MI, USA'],
-	"1_2019_awards" : False },
+	1 :{
+	    'location' : "Pontact, Michigan, USA",
+	    "rookie_year" : 1997 ,
+	    "2019_competition" : True,
+	    "2019_competition_names" : ['FIM Distric Center Line Event', 'FIM District Troy Event'],
+	    "2019_competition_location" : ['Center Line, MI,USA', 'Troy, MI, USA'],
+	    "2019_awards" : False },
 
 
-	"20_dict" :
-	   {'20_location' : "Clifton Park, New Yourk, USA",
-	"20_rookie_year" : 1992 ,
-	"20_2019_competition" : True,
-	"20_2019_competition_names" : ['New York Tech Valley Regional', 'Hudson Valley Regional', 'Curie Division'],
-	"20_2019_competition_location" : ['Troy, NY, 12180, USA', 'Suffern, NY, USA', 'Detroit, MI, USA'],
-	"20_2019_awards" : False },
+	20 :{
+	   'location' : "Clifton Park, New Yourk, USA",
+	    "rookie_year" : 1992 ,
+	    "2019_competition" : True,
+	    "2019_competition_names" : ['New York Tech Valley Regional', 'Hudson Valley Regional', 'Curie Division'],
+	    "2019_competition_location" : ['Troy, NY, 12180, USA', 'Suffern, NY, USA', 'Detroit, MI, USA'],
+	    "2019_awards" : False },
 
-	"4_dict" :
-	    {'4_location' : "Van Nuys, California, USA",
-	"4_rookie_year" : 1997,
-	"4_2019_competition" : True,
-	"4_2019_competition_names" : ['Los Angeles North Regional', 'Los Angeles Regional', 'Wings Over Camarillo', 'Batlleship Blast'],
-	"4_2019_competition_location" : ['Valencia, CA, USA', 'Los Angeles, CA, USA', 'Camarillo, CA, USA', 'San Pedro, CA, USA'],
-	"4_2019_awards" : False },
+	4 :{
+	    'location' : "Van Nuys, California, USA",
+	    "rookie_year" : 1997,
+	    "2019_competition" : True,
+	    "2019_competition_names" : ['Los Angeles North Regional', 'Los Angeles Regional', 'Wings Over Camarillo', 'Batlleship Blast'],
+	    "2019_competition_location" : ['Valencia, CA, USA', 'Los Angeles, CA, USA', 'Camarillo, CA, USA', 'San Pedro, CA, USA'],
+	    "2019_awards" : False },
 
-	"5_dict" :
-	    {'5_location' : "Melvindale, MI, USA",
-	"5_rookie_year" : 1998,
-	"5_2019_compeition" : True,
-	"5_2019_competition_names" : ['Big Bang!'],
-	"5_2019_competition_location" : ['Taylor, MI, USA'],
-	"5_2019_awards" : False },
+	5 :{
+	    'location' : "Melvindale, MI, USA",
+	    "rookie_year" : 1998,
+	    "2019_competition" : True,
+	    "2019_competition_names" : ['Big Bang!'],
+	    "2019_competition_location" : ['Taylor, MI, USA'],
+	    "2019_awards" : False },
 
-	"8_dict" :
-	    {'8_location' : 'Palo Alto, California, USA',
-	"8_rookie_year" : 1996,
-	"8_2019_compitetion" : True,
-	"8_competition_names" : ['Del Mar Regional presented by Qualcomm', 'Great Northern Regional', 'Silicon Valley Regional'],
-	"8_competition_location" : ['Del Mar, CA, USA', 'Grand Forks, ND, USA', 'San Jose, CA, USA'],
-	"8_2019_awards" : False },
+	8 :{
+	    'location' : 'Palo Alto, California, USA',
+	    "rookie_year" : 1996,
+	    "2019_compitition" : True,
+	    "2019_competition_names" : ['Del Mar Regional presented by Qualcomm', 'Great Northern Regional', 'Silicon Valley Regional'],
+	    "2019_competition_location" : ['Del Mar, CA, USA', 'Grand Forks, ND, USA', 'San Jose, CA, USA'],
+	    "2019_awards" : False },
 
 }
-print(team_dict["8_dict"])
+print("Search a team")
+team_dictionary = input("what team numbe: ")
+print("What field(location, rookie_year, 2019_competition, 2019_competition_names, 2019_competition_location, 2019_award")
+team_field = input("what field: ")
+
+print(team_dict[int(team_dictionary)][team_field])
