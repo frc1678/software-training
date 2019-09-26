46cd23e57a6c05617660b7ee382f21abd8eb0aa2
Jackson Anglin-Hill <ja61224@djusdstudents.org>
2019-09-25 19:03:21 -0700

    Edit assignment 1 again.

--- ch_1_assign_Jackson_Anglin-Hill.py
+++ ch_1_assign_Jackson_Anglin-Hill.py
@@ -1,38 +1,48 @@
 teams = {
-'1': {'location' : 'Flanders, New Jersey, USA', 
-'rookie year' : 1999, 
-'competed in 2019 competitions': 'Yes',
-'names of 2019 competitions': 'FIM District Center Line Event and FIM District Troy Event',
-'location of 2019 competitions': 'Center Line, MI, USA and Troy, MI, USA',
-'2019 season award': 'Imagery Award'},
+    '1': {
+        'location' : 'Flanders, New Jersey, USA', 
+        'rookie year' : 1999, 
+        'competed in 2019 competitions': 'Yes',
+        'names of 2019 competitions': 'FIM District Center Line Event and FIM District Troy Event',
+        'location of 2019 competitions': 'Center Line, MI, USA and Troy, MI, USA',
+        '2019 season award': 'Imagery Award'
+    },
 
-'554': {'location' : 'Ft. Thomas, Kentucky, USA', 
-'rookie year' : '2001', 
-'competed in 2019 competitions': 'Yes',
-'names of 2019 competitions': 'Miami Valley Regional',
-'location of 2019 competitions': 'Dayton, OH 45435, USA',
-'2019 season award': 'None'},
+    '554': {
+        'location' : 'Ft. Thomas, Kentucky, USA', 
+        'rookie year' : '2001', 
+        'competed in 2019 competitions': 'Yes',
+        'names of 2019 competitions': 'Miami Valley Regional',
+        'location of 2019 competitions': 'Dayton, OH 45435, USA',
+        '2019 season award': 'None'
+    },
 
-'253': {'location' : 'Millbrae, California, USA', 
-'rookie year': '1999', 
-'competed in 2019 competitions': 'Yes',
-'names of 2019 competitions': 'San Francisco Regional, Monterey Bay Regional and Newton Division',
-'location of 2019 competitions': 'San Francisco, CA, USA, Seaside, CA, USA and Houston, TX, USA',
-'2019 season award': 'Team Spirit Award'},
+    '253': {
+        'location' : 'Millbrae, California, USA', 
+        'rookie year': '1999', 
+        'competed in 2019 competitions': 'Yes',
+        'names of 2019 competitions': 'San Francisco Regional, Monterey Bay Regional and Newton Division',
+        'location of 2019 competitions': 'San Francisco, CA, USA, Seaside, CA, USA and Houston, TX, USA',
+        '2019 season award': 'Team Spirit Award'
+    },
 
-'342': {'location' : 'North Charleston, South Carolina, USA', 
-'rookie year' : '2000', 
-'competed in 2019 competitions': 'Yes',
-'names of 2019 competitions': 'Palmetto Regional and Rocket City Regional',
-'location of 2019 competitions': 'Myrtle Beach, SC 29578, USA and Huntsville, AL 35801, USA',
-'2019 season award': 'None'} ,
+    '342': {
+        'location' : 'North Charleston, South Carolina, USA', 
+        'rookie year' : '2000', 
+        'competed in 2019 competitions': 'Yes',
+        'names of 2019 competitions': 'Palmetto Regional and Rocket City Regional',
+        'location of 2019 competitions': 'Myrtle Beach, SC 29578, USA and Huntsville, AL 35801, USA',
+        '2019 season award': 'None'
+    },
 
-'16': {'location' : 'Mountain Home, Arkansas, USA', 
-'rookie year' : '1996', 
-'competed in 2019 competitions': 'Yes',
-'names of 2019 competitions': 'Midwest Regional, Rocket City Regional and Darwin Division',
-'location of 2019 competitions': 'Detroit, MI, USA and Huntsville, AL 35801, USA',
-'2019 season award': 'Industrial Design Award, Regional Finalists and Excellence in Engineering Award'}
+    '16': {
+        'location' : 'Mountain Home, Arkansas, USA', 
+        'rookie year' : '1996', 
+        'competed in 2019 competitions': 'Yes',
+        'names of 2019 competitions': 'Midwest Regional, Rocket City Regional and Darwin Division',
+        'location of 2019 competitions': 'Detroit, MI, USA and Huntsville, AL 35801, USA',
+        '2019 season award': 'Industrial Design Award, Regional Finalists and Excellence in Engineering Award'
+    }
 }
 print("You must enter options exactly as they are displayed. If they are entered incorrectly, you will not receive any information.")
 team_number = input("Please enter team number. You can enter '1, 554, 253, 342, or 16': ")
