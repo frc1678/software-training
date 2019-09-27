1bdd0f102bdd6c6ec2ffbe419b86b7da96b589af
emxia18 <emxia18@gmail.com>
2019-09-26 20:47:01 -0700

    add comments and improve code

--- ch_2_assign_emily_xia_2.py
+++ ch_2_assign_emily_xia_2.py
@@ -6,12 +6,14 @@ user_action = int(input("""Enter the number of the action you want to do
 	4	list all teams \n """))
 while True:  
 	if user_action == 0:
+	#returning to main menu
 		user_action = int(input("""Enter the number of the action you want to do
 			1	add a team
 			2	view a team
 			3	remove a team
 			4	list all teams \n """))
-	elif user_action == 1:	
+	elif user_action == 1:
+	#adding a team	
 		number = int(input("What is your team number? \n"))
 		name = str(input("What is the name of your team? \n"))
 		programming_language = str(input("What is the programming language of your team? \n"))
@@ -31,19 +33,31 @@ while True:
 		print(team_dictionary) 
 		user_action = int(input("""If you want to continue, press 1. \nIf you want to exit the "add" function, press 0. \n """))
 	elif user_action == 2:
+	#viewing a team
 		view_team = int(input("What is the number of the team you want to view? \n "))
-		print(team_dictionary[view_team])
+		if view_team in team_dictionary == True:
+			print(team_dictionary[view_team])
+		if view_team in team_dictionary == False:
+				print("I'm sorry, the team you are looking for does not exist.")
+				view_team = int(input("What is the number of the team you want to view? \n ")) 
 		user_action = int(input("""If you want to continue, press 2. \nIf you want to exit the "view" function, press 0. \n """))
 	elif user_action == 3:
+	#removing a team
 		print(team_dictionary)
 		remove_team = int(input("What is the number of the team you want to remove? \n "))
-		team_dictionary.pop(remove_team)
-		print("Team" +remove_team+ "has been removed from the team dictionary!")
+		if remove_team in team_dictionary == True:
+			team_dictionary.pop(remove_team)
+			print("Team" +remove_team+ "has been removed from the team dictionary!")
+		if remove_team in team_dictionary == False:
+				print("I'm sorry, the team you are looking for does not exist.")
+				remove_team = int(input("What is the number of the team you want to view? \n ")) 
 		user_action = int(input("""If you want to continue, press 3. \nIf you want to exit the "remove" function, press 0. \n """))
 	elif user_action == 4:
+	#listing all teams
 		print(team_dictionary)
 		user_action = int(input("""If you want to continue, press 4. \nIf you want to exit the "list" function, press 0. \n """))
 	elif user_action not in [0, 1, 2, 3, 4]:
+	#returning to main menu if user_action is not a known value
 		print("I'm sorry, could you please re-enter your command?")
 		user_action = int(input("""Enter the number of the action you want to do
 			1	add a team
