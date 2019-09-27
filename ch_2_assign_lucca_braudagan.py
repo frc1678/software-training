87234a1fae9005c0d0b416af9c7f79282ccff86a
luccambraudagan <lbraudagan@gmail.com>
2019-09-26 20:48:48 -0700

    Get rid of function

--- ch_2_assign_lucca_braudagan.py
+++ ch_2_assign_lucca_braudagan.py
@@ -1,34 +1,26 @@
 teams = {}
 
-def add_team():
+initial_user_request = input("choose_from:_'add',_'view_or_modify',_'remove',_'search',_'list',_'return'")
+if initial_user_request == "add":
 	temp_team_dictionary = {}
 
-	team_number = input("What is your team's team number?")
-	team_name = input("What is your team's name?")
-	robot_name = input("What is your robot's name?")
-	robot_p_language = input("What is your robot's programming language?")
-	robot_width = input("What is your robot's width?")
-	robot_height = input("What is your robot's height?")
-	robot_camera = input("Does your robot have a camera vision system?")
-	robot_drivetrain = input("How many drivetrain motors does your robot have?")
+	teams["team_number"] = int(input("What is your team's team number?"))
 	
-	temp_team_dictionary["team_name"] = team_name
-	temp_team_dictionary["robot_name"] = robot_name
-	temp_team_dictionary["robot_p_language"] = robot_p_language
-	temp_team_dictionary["robot_width"] = robot_width
-	temp_team_dictionary["robot_height"] = robot_height
-	temp_team_dictionary["robot_camera"] = robot_camera
-	temp_team_dictionary["robot_drivetrain"] = robot_drivetrain
+	temp_team_dictionary["team_name"] = input("What is your team's name?")
+	temp_team_dictionary["robot_name"] = input("What is your robot's name?")
+	temp_team_dictionary["robot_p_language"] = input("What is your robot's programming language?")
+	temp_team_dictionary["robot_width"] = input("What is your robot's width?")
+	temp_team_dictionary["robot_height"] = input("What is your robot's height?")
+	temp_team_dictionary["robot_camera"] = input("Does your robot have a camera vision system?")
+	temp_team_dictionary["robot_drivetrain"] = int(input("How many drivetrain motors does your robot have?"))
 	
 	teams[team_number] = temp_team_dictionary
 
-initial_user_request = input("choose_from:_'add',_'view',_'modify',_'remove',_'search',_'list',_'return'")
-if initial_user_request == "add":
-    add_team();
-elif initial_user_request == "view":
-	print("Nothing"),
-elif initial_user_request == "modify":
-	print("Nothing"),
+elif initial_user_request == "view_or_modify":
+	print(teams)
+	team_view = int(input("Which team would you like to see from these options?"))
+	if team_view in teams
+	information_category = input()
 elif initial_user_request == "remove":
 	print("Nothing"),
 elif initial_user_request == "search":
