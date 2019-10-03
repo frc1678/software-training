815a0ff24c02e7d59305df1764fa20099b87d3ea
Adam Wu <wuadam42@gmail.com>
2019-10-02 17:27:28 -0700

    plese 			if team_names in teams.val():

--- ch_2_assign_adam_wu.py
+++ ch_2_assign_adam_wu.py
@@ -1,36 +1,112 @@
-
 teams = {};
-#empty team that will be the value for given team number
+user_menu = {
+	1: 'add/change_team_info', 
+	2: 'Remove_team', 
+	3: 'search_team', 
+	4: 'show_all_teams', 
+	0: 'quit'}
 team_menu = {
-	'1': 'location',
-	'2':'program_language',
-	'3':'width', 
-	'4':'length',
-	'5':'camera_vision_system',
-	'6':'number_drivetrain_motor',
-	'0':'exit program'
-	}
-#entering team number and updating the teams dict
-team_number = int(input('Please enter team number:\n'))
-teams.update({team_number:{}})
+	1: {'property': 'team_name','type':'str'},
+	2: {'property': 'location','type':'str'},
+	3: {'property': 'program_language', 'type':'str'},
+	4: {'property': 'camera_vision_system','type':'bool'},
+	5: {'property': 'width','type':'int'}, 
+	6: {'property': 'length','type':'int'},
+	7: {'property': 'number_drivetrain_motor','type':'int'},
+	0: {'property': 'main_menu'}}
 
-user_input = '1'
-while True: 
+user_menu_selection = ''
+while user_menu_selection != 0: 
 
-	for key in team_menu.keys():
-		print(key+': '+team_menu[key])
-
-	#user_input will allow you to exit the program
-	user_input = str(input('enter number corresponding to what you would like to add/change \n'))
-	key_entered = user_input
-
-	if user_input == '0':
+	for key in user_menu.keys():
+		print(str(key) + ': ' +user_menu[key])
+			
+	user_menu_selection = str(input('please enter number of selection you would like to make \n'))
+		
+	if user_menu_selection == '0':
 		break
+	if user_menu_selection == '1':
+		team_number = int(input('Please enter team number:\n'))
+		teams.update({team_number:{}})
 
-	#val_change is the variable for new values
-	val_change = str(input('what is the new value for ' + team_menu[key_entered]+'\n'))
-	teams[team_number][team_menu[key_entered]] = user_input
+		user_input = '1'
+		while True: 
+
+			for key in team_menu.keys():
+				print(str(key) +': '+team_menu[key]['property'])
+
+				#user_input will allow you to exit the program
+			user_input = int(input('enter number corresponding to what you would like to add/change \n'))
+			key_entered = user_input
+
+			if user_input == 0:
+				break
+
+			want_to_change = team_menu[key_entered]
+
+				#val_change is the variable for new values
+			val_change = input('what is the new value for ' + want_to_change['property']+'\n')
+			try:
+				if want_to_change['type'] == 'str':
+					try:
+						if val_change.isnumeric():
+							print('please enter a number')
+					except:
+						print("Oops! please make sure you enter a " + want_to_change['type'])
+						print("Next entry.")
+
+				elif want_to_change['type'] == 'int':
+					val_change = int(val_change)
+				elif want_to_change['type'] == 'bool':
+					val_change = int(val_change)
+					val_change = val_change == 1
+				teams[team_number][want_to_change['property']] = val_change
+
+			except:
+				print("Oops! please make sure you enter a " + want_to_change['type'])
+				print("Next entry.")
+				
+			print(teams)
+
+	if user_menu_selection == '2':
+		team_type_selection = str(input('would you like to search by team name or number? \n'))
+		if team_type_selection == 'number':			
+			team_name = int(input('delete which team? \n'))
+			#if team_name in teams.values():
+			print(teams.keys())
+			teams.pop(team_name)
+			print(teams.keys())
+		else:
+			print('team ' + team_name +' does not exist')
+		if team_type_selection == 'name':
+			team_name = str(input('delete which team? \n'))
+			print(teams.values())
+
+	if user_menu_selection == '3':
+		team_search = str(input('what team would you like to search? \n'))
+		if team_search in teams.keys():
+			print("Present, ") 
+			print("value =", teams[key]) 
+		else: 
+			print("Not present") 
+
+		
+	#entering team number and updating the teams dict
+
+		   
 
 
+		"""elif want_to_change['type'] == 'int':
+			val_change = input('what is the new integer value for ' + want_to_change['property'] + '\n')
+		elif want_to_change['type'] == 'bool':
+			val_change = input('''please enter 'true' or 'false' ''' + want_to_change['property'] + '\n')"""
 
-	print(teams)
+		"""if want_to_change['type'] == 'bool':
+			val_change = input('''type "true" or "false" if they have a ''' + want_to_change['property']+'\n')
+			val_change = bool(val_change)
+		
+		if want_to_change['type'] == 'int':
+			val_change = input('please enter new number value of ' + want_to_change['property']+'\n')
+			val_change = int(val_change)"""
+
+		
