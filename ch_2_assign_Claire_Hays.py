f0c0235f5a6a4c9e08c146e429cba731dfdd2774
ClaireHays <hays.claire@gmail.com>
2019-10-02 20:48:44 -0700

    Made a system so that non-integers won't break the program.

--- ch_2_assign_Claire_Hays.py
+++ ch_2_assign_Claire_Hays.py
@@ -5,17 +5,82 @@ view team's information, modify team's information, view all teams and informati
     
 
     if command == "add a team":
-        team_num = int(input("What is the team number? ")) 
+        check = False
+        while check == False:
+            team_num = input("What is the team number? ")
+            if team_num == 'quit':
+                check = True
+            elif team_num.isdigit():
+                team_num = int(team_num)
+                check = True
+            else:
+                print("Please type your team number as an integer")
+        if team_num == 'quit':
+            continue
+
         team_name = input("What is the team name? ")
+        if team_name == 'quit':
+            continue
+
         team_prog_lang = input("What is the team's programming language? ")
-        robot_width = float(input("What is the robot's width? "))
-        robot_length = float(input("What is the robot's length? "))
-        robot_vision = input("Does the robot have camera vision? ")
-        if robot_vision.lower() == "yes":
-            robot_vision = True
-        elif robot_vision.lower() == "no":
-            robot_vision == False
-        robot_motors = int(input("How many drivetrain motors does the robot have? "))
+        if team_prog_lang == 'quit':
+            continue
+
+        check = False
+        while check == False:
+            robot_width = input("What is the robot's width? ")
+            if robot_width == 'quit':
+                check = True
+            elif robot_width.isnumeric():
+                robot_width = float(robot_width)
+                check = True
+            else:
+                print("Please type your robot width as an float")
+        if robot_width == 'quit':
+            continue
+        
+        check = False
+        while check == False:
+            robot_length = input("What is the robot's length? ")
+            if robot_length == 'quit':
+                check = True
+            elif robot_length.isnumeric():
+                robot_length = float(robot_length)
+                check = True
+            else:
+                print("Please type your robot length as an float")
+        if robot_length == 'quit':
+            continue
+
+        check = False
+        while check == False:
+            robot_vision = input("Does the robot have camera vision? ")
+            if robot_vision == 'quit':
+                check = True
+            elif robot_vision.lower() == "yes":
+                robot_vision = True
+                check = True
+            elif robot_vision.lower() == "no":
+                robot_vision == False
+                check = True
+            else:
+                print("Please type either yes or no")
+        if robot_vision == 'quit':
+            continue
+        
+        check = False
+        while check == False:
+            robot_motors = input("How many drivetrain motors does the robot have? ")
+            if robot_motors == 'quit':
+                check = True
+            elif robot_motors.isdigit():
+                robot_motors = int(robot_motors)
+                check = True
+            else:
+                print("Please type the number of motors as an integer")
+        if robot_motors == 'quit':
+            continue
+
         attributes = {"Name": team_name, "Programming Language": team_prog_lang, 
         "Width": robot_width, "Length": robot_length, "Camera Vision": robot_vision, 
         "Drivetrain Motors": robot_motors}
@@ -23,15 +88,21 @@ view team's information, modify team's information, view all teams and informati
         teams.update(new_team)
 
 
-    if command == "remove a team":
-        remove_team_num = int(input("What team number would you like to remove? "))
+    elif command == "remove a team":
+        remove_team_num = input("What team number would you like to remove? ")
+        if remove_team_num == 'quit':
+            continue
+        else:
+            remove_team_num = int(remove_team_num)
         teams.pop(remove_team_num)
         print("Team number successfully removed. To view new team list, type 'list all teams'")
 
 
-    if command == "search for a team":
+    elif command == "search for a team":
         team_search = input("What team would you like to search for? ")
-        if team_search.isnumeric() == True:
+        if team_search == 'quit':
+            continue
+        elif team_search.isnumeric() == True:
             print(teams.get(int(team_search)))
         else:
             names = []
@@ -49,59 +120,126 @@ view team's information, modify team's information, view all teams and informati
 
 
     if command == "view team's information":
-        view_team_num = int(input("What team's information would you like to view? "))
+        view_team_num = input("What team's information would you like to view? ")
+        if view_team_num == 'quit':
+            continue
+        else:
+            view_team_num = int(view_team_num)
         view_team_attribute = str(input("""What attribute would you like to view (Name, 
-        Programming Language, Width, Length, Camera Vision, Drivetrain Motors? """))
-        print(teams[view_team_num][view_team_attribute])
+    Programming Language, Width, Length, Camera Vision, Drivetrain Motors? """))
+        if view_team_attribute == 'quit':
+            continue
+        else:
+            print(teams[view_team_num][view_team_attribute])
 
 
     if command == "modify team's information":
         print("If attribute hasn't changed, type none")
-        chng_team_num = int(input("What team number's information would you like to change? "))
+        
+        check = False
+        while check == False:
+            chng_team_num = input("What team number's information would you like to change? ")
+            if chng_team_num == 'quit':
+                check = True
+            elif chng_team_num.isdigit():
+                chng_team_num = int(chng_team_num)
+                check = True
+            else:
+                print("Please type your team number as an integer")
+        if chng_team_num == 'quit':
+            continue
+
         new_team_name = input("What is the new team name? ")
+        if new_team_name == 'quit':
+           continue
+        elif new_team_name == "none":
+           new_team_name = teams[chng_team_num]["Name"]
         
-        if new_team_name == "none":
-            new_team_name = teams[chng_team_num]["Name"]
         new_team_prog_lang = input("What is the team's new programming language? ")
-        
-        if new_team_prog_lang == "none":
+        if new_team_prog_lang == 'quit':
+            continue
+        elif new_team_prog_lang == "none":
             new_team_prog_lang = teams[chng_team_num]["Programming Language"]
-        new_robot_width = input("What is the robot's new width? ")
-       
-        if new_robot_width == "none":
-            new_robot_width = teams[chng_team_num]["Width"]
-        else:
-            new_robot_width = float(new_robot_width)
-        new_robot_length = input("What is the robot's new length? ")
         
-        if new_robot_length == "none":
-            new_robot_length = teams[chng_team_num]["Length"]
-        else:
-            new_robot_length = float(new_robot_length)
-        new_robot_vision = input("Does the new robot have camera vision? ")
+        check = False
+        while check == False:
+            new_robot_width = input("What is the robot's new width? ")
+            if new_robot_width == 'quit':
+                check = True
+            elif new_robot_width == 'none':
+                new_robot_width = teams[chng_team_num]["Width"]
+                check = True
+            elif new_robot_width.isnumeric():
+                new_robot_width = float(new_robot_width)
+                check = True
+            else:
+                print("Please type your robot width as an float")
+        if new_robot_width == 'quit':
+            continue
         
-        if new_robot_vision == "none":
-            new_robot_vision = teams[chng_team_num]["Camera Vision"]
-        elif new_robot_vision.lower() == "yes":
-            new_robot_vision = True
-        elif new_robot_vision.lower() == "no":
-            new_robot_vision == False
-        new_robot_motors = input("How many drivetrain motors does the new robot have? ")
+        check = False
+        while check == False:
+            new_robot_length = input("What is the robot's new length? ")
+            if robot_length == 'quit':
+                check = True
+            elif new_robot_length == "none":
+                new_robot_length = teams[chng_team_num]["Length"]
+                check = True
+            elif robot_length.isnumeric():
+                new_robot_length = float(new_robot_length)
+                check = True
+            else:
+                print("Please type your robot length as an float")
+        if new_robot_length == 'quit':
+            continue
+
+
+        check = False
+        while check == False:
+            new_robot_vision = input("Does the new robot have camera vision? ")
+            if new_robot_vision == 'quit':
+                check = True
+            elif new_robot_vision == "none":
+                new_robot_vision = teams[chng_team_num]["Camera Vision"]
+                check = True
+            elif new_robot_vision.lower() == "yes":
+                new_robot_vision = True
+                check = True
+            elif new_robot_vision.lower() == "no":
+                new_robot_vision == False
+                check = True
+            else:
+                print("Please type either yes or no")
+        if new_robot_vision == 'quit':
+            continue
         
-        if new_robot_motors == "none":
-            new_robot_motors = teams[chng_team_num]["Drivetrain Motors"]
-        else:
-            new_robot_motors = int(new_robot_motors)
+        
+        check = False
+        while check == False:
+            new_robot_motors = input("How many drivetrain motors does the new robot have? ")
+            if new_robot_motors == 'quit':
+                check = True
+            elif new_robot_motors == "none":
+                new_robot_motors = teams[chng_team_num]["Drivetrain Motors"]
+                check = True
+            elif new_robot_motors.isdigit():
+                new_robot_motors = int(new_robot_motors)
+                check = True
+            else:
+                print("Please type the number of motors as an integer")
+        if new_robot_motors == 'quit':
+            continue
+
         new_attributes = {"Name": new_team_name, "Programming Language": new_team_prog_lang, 
         "Width": new_robot_width, "Length": new_robot_length, "Camera Vision": new_robot_vision, 
         "Drivetrain Motors": new_robot_motors}
         teams[chng_team_num] = new_attributes
     
 
-    if command == "view all teams and information":
+    elif command == "view all teams and information":
         print(teams)
     
 
-    if command == "quit":
+    elif command == "quit":
         break
 
