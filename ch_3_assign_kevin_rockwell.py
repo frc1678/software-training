dfc0eebd99de420bbdb4b2a0c5a098b4f5b146b0
kevinrockwell <kevinnrockwell@gmail.com>
2019-10-15 21:35:35 -0700

    Add basic input functions and general program structure

---
+++ ch_3_assign_kevin_rockwell.py
@@ -0,0 +1,57 @@
+from ch_1_assign_kevin_rockwell import teams
+
+def input_or_cancel(prompt):
+    i = input(prompt + ):
+    if i == "q":
+        return None
+    else:
+        return i
+
+def get_int():
+    while True:
+        i = input_or_cancel("Please enter an integer or 'q' to cancel"):
+        if i is not None:
+            if i.isdigit():
+                return int(i)
+            else:
+                print(f"Invalid input {i}")
+                continue
+        return i # user canceled, so return none
+
+while True:
+    #Main Menu
+    print("""
+        Allowed Actions:
+        (a)dd Team
+        (v)iew team data
+        (m)odify team data
+        (r)emove team
+        (s)earch for teams
+        (l)ist all teams
+        (e)xit
+        """)
+    selection = ""
+    selection = input("Action: ")
+    if selection == "a": #Add team
+        pass
+
+    elif selection == "v": #View team
+        pass        
+
+    elif selection == "m": #modify team
+        pass
+
+    elif selection == "r": #remove team
+        pass
+
+    elif selection == "s": #search for teams
+        pass
+
+    elif selection == "l": #list teams
+        pass
+
+    elif selection == "e": #exit program
+        pass
+
+    else:
+        print(f"Invalid Action: {selection}\n")
