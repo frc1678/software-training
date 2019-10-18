1a0a0d33f91d38a669ef5864608885ce91e1191d
Adam Wu <wuadam42@gmail.com>
2019-10-17 18:54:21 -0700

    Change all if statements to functions

--- ch_3_assign_adam_wu.py
+++ ch_3_assign_adam_wu.py
@@ -1,3 +1,83 @@
+def team_add_function():
+    team_number = input('Please enter team number:\n')
+    if team_number.isnumeric():
+        teams.update({team_number:{}})
+    else:
+        print('please enter a number')
+        
+        return
+
+    user_input = '1'
+    while True: 
+
+
+        for key in team_menu.keys():
+            print(str(key) +': '+team_menu[key]['property'])
+
+            #user_input will allow you to exit the program
+        user_input = int(input('enter number corresponding to what you would like to add/change \n'))
+        key_entered = user_input
+
+        if user_input == 0:
+            break
+
+        want_to_change = team_menu[key_entered]
+
+            #val_change is the variable for new values
+        val_change = input('what is the new value for ' + want_to_change['property']+'\n')
+        try:
+            if want_to_change['type'] == 'str':
+                try:
+                    if val_change.isnumeric():
+                        print('please enter a number')
+                except:
+                    print("Oops! please make sure you enter a " + str(want_to_change['type']))
+                    print("Next entry.")
+
+            elif want_to_change['type'] == 'int':
+                val_change = int(val_change)
+            elif want_to_change['type'] == 'bool':
+                val_change = int(val_change)
+                val_change = val_change == 1
+            teams[team_number][want_to_change['property']] = val_change
+
+        except:
+            print("Oops! please make sure you enter a " + want_to_change['type'])
+            print("Next entry.")
+            
+        print(teams)
+
+def team_delete_function():
+    team_name = input('delete which team? \n')
+    if team_name.isnumeric():
+        if team_name in teams.keys():
+            print(teams.keys())
+            teams.pop(team_name)
+            print(teams.keys())
+        else:
+            print('team ' + team_name +' does not exist')
+    else:
+        print('please enter an integer \n')
+
+def team_search_function():
+    team_search = input('what team would you like to search? \n')
+    #team_search_validation = False
+    for team_number, team_name in teams.items():
+        if team_search == team_number:
+            for sub_team_name, sub_team_value in team_name.items():
+                print(sub_team_name + ' = ' + sub_team_value)
+    for team_number, team_name in teams.items():
+        if team_search == team_name['team_name']:
+            for sub_team_number, sub_team_name in team_name.items():
+                print(str(sub_team_number) + ' = ' + str(sub_team_name))
+        
+
+def teams_show_function():
+    for key, value in teams.items():
+        print (key)
+        for subkey, subvalue in value.items():
+            print('  '+ str(subkey) + ' = ' + str(subvalue))
+
 teams = {};
 user_menu = {
     1: 'add/change_team_info', 
@@ -18,88 +98,18 @@ team_menu = {
 user_menu_selection = ''
 while user_menu_selection != 0: 
 
-    for key in user_menu.keys():
-        print(str(key) + ': ' +user_menu[key])
+    for key, value in user_menu.items():
+        print(str(key) + ': ' + str(value))
             
     user_menu_selection = str(input('please enter number of selection you would like to make \n'))
         
     if user_menu_selection == '0':
         break
     if user_menu_selection == '1':
-        team_number = input('Please enter team number:\n')
-        if team_number.isnumeric():
-            teams.update({team_number:{}})
-        else:
-            print('please enter a number')
-            
-            continue
-    
-        user_input = '1'
-        while True: 
-
-
-            for key in team_menu.keys():
-                print(str(key) +': '+team_menu[key]['property'])
-
-                #user_input will allow you to exit the program
-            user_input = int(input('enter number corresponding to what you would like to add/change \n'))
-            key_entered = user_input
-
-            if user_input == 0:
-                break
-
-            want_to_change = team_menu[key_entered]
-
-                #val_change is the variable for new values
-            val_change = input('what is the new value for ' + want_to_change['property']+'\n')
-            try:
-                if want_to_change['type'] == 'str':
-                    try:
-                        if val_change.isnumeric():
-                            print('please enter a number')
-                    except:
-                        print("Oops! please make sure you enter a " + want_to_change['type'])
-                        print("Next entry.")
-
-                elif want_to_change['type'] == 'int':
-                    val_change = int(val_change)
-                elif want_to_change['type'] == 'bool':
-                    val_change = int(val_change)
-                    val_change = val_change == 1
-                teams[team_number][want_to_change['property']] = val_change
-
-            except:
-                print("Oops! please make sure you enter a " + want_to_change['type'])
-                print("Next entry.")
-                
-            print(teams)
-
-    if user_menu_selection == '2':  
-        team_name = input('delete which team? \n')
-        if team_name.isnumeric():
-            if team_name in teams.keys():
-                print(teams.keys())
-                teams.pop(team_name)
-                print(teams.keys())
-            else:
-                print('team ' + team_name +' does not exist')
-        else:
-            print('please enter an integer \n')
-
+        team_add_function()
+    if user_menu_selection == '2':
+        team_delete_function()
     if user_menu_selection == '3':
-        team_search = input('what team would you like to search? \n')
-        #team_search_validation = False
-        for team_number, team_name in teams.items():
-            if team_search == team_number:
-                for sub_team_name, sub_team_value in team_name.items():
-                    print(sub_team_name + ' = ' + sub_team_value)
-        for team_number, team_name in teams.items():
-            if team_search == team_name['team_name']:
-                for sub_team_number, sub_team_name in team_name.items():
-                    print(str(sub_team_number) + ' = ' + str(sub_team_name))
-            
+        team_search_function()
     if user_menu_selection == '4':
-        for key, value in teams.items():
-            print (key)
-            for subkey, subvalue in value.items():
-                print('  '+ subkey + ' = ' + subvalue)
+        teams_show_function()
