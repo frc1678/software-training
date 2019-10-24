8038332c17144eb3d905b3cae87b2c70b00a80b6
azarins1 <az61768@djusdstudents.org>
2019-10-23 20:33:58 -0700

    Added a bonus project: Rubik's cubes! This program tells you the number of combonations (pardon the spelling) from a 2x2 to Infinity x Infinity cubes! Have fun learning how the cubes progressively become more advanced! :)

--- ch_4_assign_Arthur_Zarins.py
+++ ch_4_assign_Arthur_Zarins.py
@@ -1,21 +1,36 @@
 import math
-class BasicPoint:
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
     
     def distance(self):
-        return math.sqrt(self.x **2 + self.y **2)
+        if (is_number(self.x) and is_number(self.y)):
+            return math.sqrt(self.x **2 + self.y **2)
+        else:
+            return -1
     
-class AwesumPointz(BasicPoint):
+class Point3d(Point):
     def __init__(self, x, y, z):
         super().__init__(x, y)
         self.z = z
 
     def distance(self):
-        return math.sqrt(self.z * self.z + math.sqrt(self.x **2 + self.y **2) **2)
+        if (is_number(self.x) and is_number(self.y) and is_number(self.z)):
+            return math.sqrt(self.z **2 + super().distance() ** 2)
+        else:
+            return -1
 
-boringPoint2d = BasicPoint(3, 4)
-coolPoint = AwesumPointz(3, -4, 4)
+boringPoint2d = Point(3, 4)
+coolPoint = Point3d(3, -4, 4)
+wrong = Point3d("q", -4, 4)
 print(boringPoint2d.distance())
 print(coolPoint.distance())
+print(wrong.distance())
+#We know the last one's bogus because it's value is -1
