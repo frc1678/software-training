2139d6cb5c194beeefc20e54bdee927a15a1e611
luccambraudagan <lbraudagan@gmail.com>
2019-10-24 19:47:24 -0700

    Finish assignment

--- ch_4_assign_lucca_braudagan.py
+++ ch_4_assign_lucca_braudagan.py
@@ -2,20 +2,20 @@ from math import sqrt
 
 class Point:
 	def __init__(self, x_value, y_value):
-		self.x_value = x
-		self.y_value = y
+		self.x_value = x_value
+		self.y_value = y_value
 
 	def distance(self):
-		return sqrt(x ** 2 + y ** 2)
+		return sqrt(x_value ** 2 + y_value ** 2)
 
 class Point_3D(Point):
 	def __init__(self, x_value, y_value, z_value):
-		super().__init__(x, y)
-		self.z_value = z
+		super().__init__(x_value, y_value)
+		self.z_value = z_value
 
 	def distance(self):
 		distance_2D = super().distance()
-		return sqrt(distance_2D ** 2 + z ** 2)
+		return sqrt(distance_2D ** 2 + z_value ** 2)
 
 def int_validation(prompt):
 	while True:
@@ -30,6 +30,8 @@ x_value = int_validation(x_value_2D)
 y_value_2D = input("What is the y-value of your 2D point? ")
 y_value = int_validation(y_value_2D)
 
+point_2D = Point(x_value, y_value)
+point_2D_distance = point_2D.distance()
 
 x_value_3D = input("What is the x-value of your 3D point? ")
 x_value = int_validation(x_value_3D)
@@ -39,4 +41,34 @@ y_value = int_validation(y_value_3D)
 
 z_value_3D = input("What is the z-value of your 3D point? ")
 z_value = int_validation(z_value_3D)
+
+point_3D = Point_3D(x_value, y_value, z_value)
+
+point_3D_distance = point_3D.distance()
+
+if point_2D_distance > point_3D_distance:
+	print(point_2D_distance)
+	print("is greater than")
+	print(point_3D_distance)
+	print("The distance of the 2D point in farther from the origin than the 3D point.")
+
+elif point_2D_distance < point_3D_distance:
+	print(point_2D_distance)
+	print("is less than")
+	print(point_3D_distance)
+	print("The distance of the 2D point in closer to the origin than the 3D point.")
+
+elif point_2D_distance == point_3D_distance:
+	print(point_2D_distance)
+	print("is equal to")
+	print(point_3D_distance)
+	print("The 2D point and 3D point are equal distance from the origin.")
+
+else:
+	print("Something went wrong. The 2D point distance is not greater than, less than, or equal to the 3D point distance from the origin.")
+
+
+
+
+
 
