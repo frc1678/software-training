8d28dacc01577a57145445b6f715b8663f0fb34b
jonathanhoNumberXIII <jonathanbho@gmail.com>
2019-10-24 19:59:54 -0700

    In this bundle of code, I'm using the Pythagorean Theorem for two-dimensional and three-dimensional shapes, mainly rectangular prisms and 2D triangles. The user is in a while loop while doing this, and he or she can exit when they type "exit". Unless they don't want to, they can enter their own numbers and allow the code to use the Pythagorean Theorem to calculate the distance between one point and another in the dimensional shape they choose.

---
+++ ch_4_assign_Jonathan_Ho.py
@@ -0,0 +1,56 @@
+from math import sqrt
+
+# 2D Pythagorean Theorem
+class Point():
+	def __init__(self, x, y):
+		self.x = float(x)
+		self.y = float(y)
+
+	# Pythagorean Theorem
+	def Distance(self):
+		hypo = self.x ** 2 + self.y ** 2
+		return sqrt(hypo)
+
+# Three Dimensional Pythagorean Theorem
+class Point_3D(Point):
+	def __init__(self, x, y, z):
+		self.z = float(z)
+		super().__init__(x, y)
+		
+
+	def Distance(self):
+		hypo_2 = super().Distance() ** 2 + self.z ** 2	
+		return sqrt(hypo_2)
+
+
+while True:
+	Operation = input("What type of dimension do you want to use for the Pythagorean Theorem? If you wish to leave, type 'exit'.")
+	if Operation == "2D":
+		x = input("Enter the first value of your first leg of your triangle.")
+		y = input("Enter the second value of your next leg.")
+		# 
+		# Assure if x and y are integers
+		if x.isdigit() and y.isdigit():
+			point = Point(x, y).Distance()
+			print(point)
+		else:
+			print("Sorry, the variable is not a number. Try again.")
+
+	
+	elif Operation == "3D":
+		x = input("Enter the first value of the length of the side of your 3D rectangle.")
+		y = input("Enter the next value of the second length of your 3D rectangle.")
+		z = input("Now enter the height of your 3D rectangle")
+		# Validates if the inputs are integers for the code to use the if statement
+		if x.isdigit() and y.isdigit() and z.isdigit():
+			calc_3D_point = Point_3D(x, y, z).Distance()
+			print(calc_3D_point)
+		else:
+			print("Sorry, your variable wasn't an integer. Try using a number.")
+	# If the user wishes to leave
+	elif Operation == "exit":
+		print("So long! Till we meet again.")
+		break;
+	# If the user types it incorrectly
+	else:
+		print("Sorry. Your response wasn't valid. Try 3D or 2D.")
