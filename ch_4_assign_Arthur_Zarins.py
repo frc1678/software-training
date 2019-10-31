600d3832ea71cbe2128f41a809a0eff193b171f3
azarins1 <az61768@djusdstudents.org>
2019-10-31 16:46:46 -0700

    .

--- ch_4_assign_Arthur_Zarins.py
+++ ch_4_assign_Arthur_Zarins.py
@@ -55,5 +55,5 @@ while True:
     print("The distance from the 2d point to orgin: " +
           str(boringPoint2d.distance()))
     print("The distance from the 3d point to orgin: " + str(coolPoint.distance()))
-
+            
 # We know the last one's bogus because it's value is -1
