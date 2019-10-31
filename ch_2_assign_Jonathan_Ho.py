8f03d32a6293f21cca2ae43306dddb74e5e9c8b0
jonathanhoNumberXIII <jonathanbho@gmail.com>
2019-10-30 20:39:11 -0700

    This code has been updated and modified to make sure that there are no error messages while maintaining its primary function. This is an updated version of Chapter 2's assignmet

--- ch_2_assign_Jonathan_Ho.py
+++ ch_2_assign_Jonathan_Ho.py
@@ -1,13 +1,18 @@
 teams = []
 
-Robot_Team_A = {"1678": {'Number': '1678', 'Name': 'Citrus Circuits', 'Prog. Language': 'Java', 'Width': '15 inches', 'Length': '16 inches', 'Camera Vision?': 'Does not have camera vision system',
+Robot_Team_A = {'1678': {'Number': '1678', 'Name': 'Citrus Circuits', 'Prog. Language': 'Java', 'Width': '15 inches', 'Length': '16 inches', 'Camera Vision?': 'Does not have camera vision system',
 	'Drivetrain Motors': '12 drivetrain motors'}}
-Robot_Team_B = {"744": {'Number': '744', 'Name': 'Shark Attack', 'Prog. Language': 'C++', 'Width': '13 inches', 'Length': '15 inches', 'Camera Vision?': 'Does have camera vision system',
+Robot_Team_B = {'744': {'Number': '744', 'Name': 'Shark Attack', 'Prog. Language': 'C++', 'Width': '13 inches', 'Length': '15 inches', 'Camera Vision?': 'Does have camera vision system',
 	'Drivetrain Motors': '10 drivetrain motors'}}
-Robot_Team_C = {"5852": {'Number': '5852', 'Name': 'Illusion Robotics', 'Prog. Language': 'C+', 'Width': '17 inches', 'Length': '16 inches', 'Camera Vision?': 'Does not have camera video system',
+Robot_Team_C = {'5852': {'Number': '5852', 'Name': 'Illusion Robotics', 'Prog. Language': 'C+', 'Width': '17 inches', 'Length': '16 inches', 'Camera Vision?': 'Does not have camera video system',
 	'Drivetrain Motors': '9 drivetrain motors'}}
 
-Robot_Team_all = {'1678': Robot_Team_A, '744': Robot_Team_B, '5852': Robot_Team_C}
+Robot_Team_all = {'1678': {'Number': '1678', 'Name': 'Citrus Circuits', 'Prog. Language': 'Java', 'Width': '15 inches', 'Length': '16 inches', 'Camera Vision?': 'Does not have camera vision system',
+	'Drivetrain Motors': '12 drivetrain motors'},
+	'744': {'Number': '744', 'Name': 'Shark Attack', 'Prog. Language': 'C++', 'Width': '13 inches', 'Length': '15 inches', 'Camera Vision?': 'Does have camera vision system',
+	'Drivetrain Motors': '10 drivetrain motors'},
+	'5852': {'Number': '5852', 'Name': 'Illusion Robotics', 'Prog. Language': 'C+', 'Width': '17 inches', 'Length': '16 inches', 'Camera Vision?': 'Does not have camera video system',
+	'Drivetrain Motors': '9 drivetrain motors'}}
 
 def list_teams(list_of_teams):
 	for team in list_of_teams:
@@ -22,29 +27,42 @@ def add():
 def remove():
 	Player_Delete = input("Let's see. What team do you want to delete?")
 	if Player_Delete.isalpha and Player_Delete != None:
-		teams.remove(Player_Delete)
-		print(teams)
+		if Player_Delete in teams:
+			teams.remove(Player_Delete)
+			print(teams)
+		else:
+			print("Sorry. That's not in the database. Try again.")
 
 def view():
 	view_input = input("Whose data do you want to view?: ")
 	team_information_input = input("What datapoint would you like to see?: ")
-	print(Robot_Team_all[view_input][view_input][team_information_input])
+	if view_input in Robot_Team_all and team_information_input in Robot_Team_all[view_input]:	
+		print(Robot_Team_all[view_input][team_information_input])
+	else:
+		print("Sorry. I don't think your value is in the data files. Make sure your values are strings and try again.")
 
 def modify():
-	modifier_team = input("\n team's data would you like to modify?: ")
+	modifier_team = input("\n Whose team's data would you like to modify?: ")
 	modifier_datapoint = input("\nWhat datapoint would you like to modify?: ")
-	print("The current value for " + str(modifier_datapoint) + " of team " + str(modifier_team) + " is " + Robot_Team_all[modifier_team][modifier_team][modifier_datapoint])
-	modifiee_datapoint = input("\nWhat value would you like to change it to?: ")
-	Robot_Team_all[modifier_team][modifier_team][modifier_datapoint] = modifiee_datapoint
-	print("The new value has been changed to: " + str(modifiee_datapoint))
+	if modifier_team in Robot_Team_all and modifier_datapoint in Robot_Team_all[modifier_team]:
+		print("The current value for " + str(modifier_datapoint) + " of team " + str(modifier_team) + " is " + Robot_Team_all[modifier_team][modifier_datapoint])
+		modifiee_datapoint = input("\nWhat value would you like to change it to?: ")
+		Robot_Team_all[modifier_team][modifier_datapoint] = modifiee_datapoint
+		print("The new value has been changed to: " + str(modifiee_datapoint))
+	else:
+		print("Sorry. I'm afraid your input is not in the database. Try again")
 
 def search():
-	search_team = input("Which team do you want to look for?")
-	print("Is " + str(search_team) + " in your list of teams?: " + str(search_team in teams));
+	search_team = input("Which team do you want to look for? ")
+	name_team = input("What is the name of the team, for safety measures? ")
+	if search_team in Robot_Team_all and name_team in Robot_Team_all[search_team]['Name']:
+		print(str(search_team in Robot_Team_all))
+	else:
+		print(False)
 
 
 while True: 
-	Player_Use = input("Hello, User. Welcome to the FIRST Robotics Team Menu! What would you like to do?")
+	Player_Use = input("Hello, User. Welcome to the FIRST Robotics Team Menu! What would you like to do? ")
 	if Player_Use == 'add' or Player_Use == 'Add':
 		add()
 	elif Player_Use == 'view' or Player_Use == 'View':
@@ -58,8 +76,7 @@ while True:
 	elif Player_Use.lower() == "view all teams":
 		list_teams(teams)
 	elif Player_Use == "quit":
+		print("Thanks for using this! See ya real soon!")
 		break;
 	else:
 		print("Sorry! I didn't get that! Please try again.")
-
-
