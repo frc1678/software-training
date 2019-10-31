b98610d00c15969985cbbfa54c4f8e1139645da2
azarins1 <az61768@djusdstudents.org>
2019-10-30 19:58:13 -0700

    Updated chapter 4

--- ch_4_assign_Arthur_Zarins.py
+++ ch_4_assign_Arthur_Zarins.py
@@ -1,21 +1,26 @@
 import math
+
+
 def is_number(s):
     try:
         float(s)
         return True
     except ValueError:
         pass
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
+
     def distance(self):
         if (is_number(self.x) and is_number(self.y)):
-            return math.sqrt(self.x **2 + self.y **2)
+            return math.sqrt(float(self.x) ** 2 + float(self.y) ** 2)
         else:
             return -1
-    
+
+
 class Point3d(Point):
     def __init__(self, x, y, z):
         super().__init__(x, y)
@@ -23,14 +28,32 @@ class Point3d(Point):
 
     def distance(self):
         if (is_number(self.x) and is_number(self.y) and is_number(self.z)):
-            return math.sqrt(self.z **2 + super().distance() ** 2)
+            return math.sqrt(float(self.z) ** 2 + super().distance() ** 2)
         else:
             return -1
 
-boringPoint2d = Point(3, 4)
-coolPoint = Point3d(3, -4, 4)
-wrong = Point3d("q", -4, 4)
-print(boringPoint2d.distance())
-print(coolPoint.distance())
-print(wrong.distance())
-#We know the last one's bogus because it's value is -1
+
+while True:
+    a1 = input("give the x value of the 2d point \n")
+    a2 = input("give the y value of the 2d point\n")
+    a3 = input("give the x value of the 3d point\n")
+    a4 = input("give the y value of the 3d point\n")
+    a5 = input("give the z value of the 3d point\n")
+    boringPoint2d = Point(a1, a2)
+    coolPoint = Point3d(a3, a4, a5)
+
+    if boringPoint2d.distance() == -1:
+        print("The 2d point has arguements that are not real numbers")
+    if coolPoint.distance() == -1:
+        print("The 3d point has arguements that are not real numbers")
+    if boringPoint2d.distance() == coolPoint.distance():
+        print("They have equal distances")
+    if boringPoint2d.distance() > coolPoint.distance():
+        print("The 2d point's furthur away from the orgin")
+    else:
+        print("The 3d point's furthur away from the orgin")
+    print("The distance from the 2d point to orgin: " +
+          str(boringPoint2d.distance()))
+    print("The distance from the 3d point to orgin: " + str(coolPoint.distance()))
+
+# We know the last one's bogus because it's value is -1
