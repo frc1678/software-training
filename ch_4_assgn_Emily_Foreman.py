f1f1102f615f7f9d6244c677e3be4f48cfddbcab
Emily Foreman <emily_foreman@icloud.com>
2019-11-06 20:26:33 -0800

    Change variable names, added comparasion if equal

--- ch_4_assgn_Emily_Foreman.py
+++ ch_4_assgn_Emily_Foreman.py
@@ -1,54 +1,55 @@
 import math
 
-class Distanceorigin:
-    def __init__ (self, xx, yy):
-        self.newthing = xx
-        self.y = yy
+class Two_D_Point:
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y
     def distance():
         #dist from origin is py theorm : w/ 0,0
       #  partone = x - 0
-        partone = newthing - 0
-        partwo = y - 0
-        calculate = partone**2 + partwo**2
+        part_one_calculation = x - 0
+        part_two_calculation = y - 0
+        calculate = part_one_calculation**2 + part_two_calculation**2
         lastpart = math.sqrt(calculate)
         print("The distance from the origin is: ")
         print(lastpart)
         return lastpart
-class ThreeD(Distanceorigin):
+class ThreeD(Two_D_Point):
     def __init__ (self, x, y,z):
         self.z = tdz
     def distance():
-        partone =x**2 + y**2 + z**2
-        parttwo = math.sqrt(partone)
+        part_one_calculation =x**2 + y**2 + z**2
+        part_two_calculation = math.sqrt(part_one_calculation)
         print("The distance from the origin is: ")
-        print(parttwo)
-        return parttwo
+        print(part_two_calculation)
+        return part_two_calculation
 
 check = False
 while check == False:
     x = input("What is the x cordinate? ")
-    ycord = input("What is the y cordinate? ")
-    if x.isdigit() and ycord.isdigit():
-        newthing = float(x)
-        y = float(ycord)
+    y = input("What is the y cordinate? ")
+    if x.isdigit() and y.isdigit():
+        x = float(x)
+        y = float(y)
         check = True
-a = int(Distanceorigin.distance())
-
+Distance  = int(Two_D_Point.distance())
 checktwo = False
 while checktwo == False:
-    threedx = input("What is the x value of the 3D point? ")
-    threedy = input("What is the y value of the 3D point? ")
-    threedz = input("What is the z value of the 3D point? ")
-    if threedx.isdigit() and threedy.isdigit() and threedz.isdigit():
-        x = int(threedx)
-        y = int(threedy)
-        z = int(threedz)
+    TDx = input("What is the x value of the 3D point? ")
+    TDy = input("What is the y value of the 3D point? ")
+    TDz = input("What is the z value of the 3D point? ")
+    if TDx.isdigit() and TDy.isdigit() and TDz.isdigit():
+        x = int(TDx)
+        y = int(TDy)
+        z = int(TDz)
         checktwo = True
 
-b = int(ThreeD.distance())
-def comparison(a, b):
-    if a > b:
+ThreeD_Distance = int(ThreeD.distance())
+def comparison(Distance, ThreeD_Distance):
+    if Distance > ThreeD_Distance:
         print("The 2D point is farther from the origin")
-    if a < b:
+    elif Distance < ThreeD_Distance:
         print("The 3D point is farther from the origin")
-comparison(a, b)
+    elif Distance == ThreeD_Distance:
+        print("The points are equal in distance")
+comparison(Distance, ThreeD_Distance)
