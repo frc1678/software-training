36e1772b70db6745aabd7acfea9edfa95a1868b7
Olivia Sagayaradj <olivia@Olivias-MacBook-Air.local>
2019-11-06 20:27:55 -0800

    fixed errors

--- chapter_4_lesson_sagayaradj.py
+++ chapter_4_lesson_sagayaradj.py
@@ -1,24 +1,28 @@
-user_question_x = int(input('Type value for x.'))
-user_question_y = int(input('Type value for y.'))
-user_question_z = int(input('Type value for z.'))
+user_question_x_point_x = int(input('Type value for x for point x.'))
+user_question_y_point_x = int(input('Type value for y for point x.'))
+user_question_x_point_y = int(input('Type value for x for point y.'))
+user_question_y_point_y = int(input('Type value for y for point y.'))
+user_question_x_point_z = int(input('Type value for x for point z.'))
+user_question_y_point_z = int(input('Type value for y for point z.'))
+user_question_z_point_z = int(input('Type value for z for point z.'))
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    def distance(self, x, y):
+    def distance(self):
         import math
         print(math.sqrt(self.x**2 + self.y**2))
-x = user_question_x
-y = user_question_y
+x = Point(user_question_x_point_x,user_question_y_point_x)
+y = Point(user_question_x_point_y,user_question_y_point_y)
 class Point_3D(Point):
         def __init__(self, x, y, z):
-            Point.__init__(self, x, y, z)
+            super().__init__(x, y)
             self.z = z
-        def distance(self, x, y, z):
+        def distance_3D(self):
             import math
             print(math.sqrt(self.x**2 + self.y**2 + self.z**2))
-z = user_question_z
-Point_3D.distance(user_question_x, user_question_y, user_question_z)
+x = Point_3D(user_question_x_point_z,user_question_y_point_z,user_question_z_point_z)
+x.distance_3D()
 user_points_question = input("Which 2D point would you like to use to find the distance from the 3D point, 'x' or 'y'")
 print("act: " + str(user_points_question))
 if user_points_question == 'x':
