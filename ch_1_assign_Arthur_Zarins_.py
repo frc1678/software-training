2683c7613c80eb9fabf90e3ac268274bd2db4d5f
azarins1 <az61768@djusdstudents.org>
2019-11-07 16:56:52 -0800

    Fixed chapter 1

--- ch_1_assign_Arthur_Zarins_.py
+++ ch_1_assign_Arthur_Zarins_.py
@@ -3,31 +3,38 @@ teams = {
     "1678": {'Last Year': 2019,
              'Location': "Davis CA",
              'Rookie Year': 2005,
-             'Events': "Sac Regional, Central Valley Regional, AeroSpace Valley Regional, Carver division, Einstein field, RCC Qianjiang International Robotics Invitational, Chezy Champs"
+             'Events': "Sac Regional, Central Valley Regional, AeroSpace Valley Regional, Carver division, Einstein field, RCC Qianjiang International Robotics Invitational, Chezy Champs",
+             'Awards': "Capital City Classic 2019, 2018 Madtown Finalist, 2018 Chezy Champs Finalist"
              },
     "2551": {'Last Year': 2019,
              'Location': "Novato CA",
-             'Rookie Year': 2008, 'Events':
-                 'Sac Regional, SF Regional, Galileo divison'},
+             'Rookie Year': 2008, 
+             'Events': 'Sac Regional, SF Regional, Galileo divison',
+             'Awards': "UC Davis highest rookie seed award, 2018 Sac regional innovation in control"
+             },
     "3421": {'Last Year': 2013,
              'Location': "Marysville MI",
              'Rookie Year': 2010,
-             'Events': 'MI FRC state championship'},
+             'Events': 'MI FRC state championship',
+             'Awards': "Liviona district 2012 cooperation award"},
     "2942": {'Last Year': 2018,
              'Location': "Seattle, WA",
              'Rookie Year': 2009,
-             'Events': ''},
+             'Events': 'Seattle',
+             'Awards': "2009 Seattle Imagery award"},
     "3890": {'Last Year': 2015,
              'Location': "Boulder, Colorado",
              'Rookie Year': 2003,
-             'Events': 'Colorado Regional'}
+             'Events': 'South pacific regional',
+             'Awards': "South pacific regional Woodie Flowers award"},
+
 }
 #reference = "is"
 
 
 def teamStats(teamName, attribute):
     # New variable refernce will handle grammer related to pluralities
-    if (attribute == "Events" or  attribute == "Rookie Year" or  attribute == "Location" or attribute == "Last Year"):
+    if (attribute == "Events" or attribute == "Rookie Year" or attribute == "Location" or attribute == "Last Year" or attribute == "Awards"):
         if(teamName in teams):
             reference = "is"
             stringTest = str(teams[teamName][attribute]).split(",")
@@ -40,15 +47,11 @@ def teamStats(teamName, attribute):
                 reference = "is"
 
             print("Team " + str(teamName) + "'s " + str(attributeDisplay) +
-                    " " + str(reference) + " " + str(teams[teamName][attribute]))
-
-
-# The big test, comprised of 3 questions:
-teamStats("1678", 'Location')
-teamStats("1678", 'Events')
-# Just one event: demonstration of proper grammer
-teamStats("3421", 'Events')
-
+                  " " + str(reference) + " " + str(teams[teamName][attribute]))
+        else:
+            print("That team does not exist")
+    else:
+        print("That stat does not exist")
 
 def runContinuous():
     while(True):
@@ -56,6 +59,7 @@ def runContinuous():
         a2 = input("What's the stat?\n      ")
         teamStats(a1, a2)
 
-#If you want to run the program, uncomment the following line (commented so chapter 3 can work):
-#runContinuous()
+
+# If you want to run the program, uncomment the following line (commented so chapter 3 can work):
+# runContinuous()
 runContinuous()
