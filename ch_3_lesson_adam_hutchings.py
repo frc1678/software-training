81257d039961df1204e084d57e279a86cd5fb9ea
Adam Hutchings <adamhutchings@Adams-MacBook-Pro.local>
2019-11-09 20:01:17 -0800

    Delete semicolons, add menu quit option.

--- ch_3_lesson_adam_hutchings.py
+++ ch_3_lesson_adam_hutchings.py
@@ -8,13 +8,13 @@ def remove_function():
         removal = input("Which team would you like to remove? This action cannot be undone!")
         if ("statistics" + removal) in master_dictionary:
             master_dictionary[removal] = {}
-            break;
+            break
 
 def view_function():
     while True:
         viewing_selection = input("Which team's statistics would you like to view? ")
         if "statistics" + str(viewing_selection) in master_dictionary:
-            break;
+            break
     while True:
         statistic_selection = input("""Which statistic would you like to view? Please type one of
         tm (team number), n (name), pl (programming language), w or l (width or a length for your robot), 
@@ -24,20 +24,20 @@ def view_function():
             break;
         elif master_dictionary["statistics" + viewing_selection][statistic_selection] == '':
             print("That hasn't been added yet. Try something else.")
-            break;
+            break
 
 def search_teams(): #Searching teams
     while True:
         team_search = input("What team would you like to search for? ")
         if team_search.isdigit() == False: #If the search is all digits
             print("Oops, enter a valid input!")
-            continue;
+            continue
         else:
-            break;
+            break
     for search in master_dictionary: #Checking every master_dict key to see if it's equal to the search
         if search == ("statistics" + team_search):
             return ("A match has been found!")
-            break;
+            break
     return ("No matches were found.")
 
 def add_team(): #How to add a new team
@@ -46,12 +46,12 @@ def add_team(): #How to add a new team
         team_name = input("What would you like your team to be named? Make sure it's all numbers! ") #Getting a name for the team
         if team_name.isdigit() == False: #Checking if a team name is all digits
             print("Valid input, please!")
-            continue;
+            continue
         else:
             master_dictionary["statistics" + str(team_name)] = {} #Making a dictionary entry for the team
             for yeet in statistic_list:
                 master_dictionary["statistics" + str(team_name)][yeet] = ''
-            break;
+            break
 
 def add_statistic(): #How to add a statistic for a team
     team_name = '' #Setting variables empty
@@ -59,14 +59,14 @@ def add_statistic(): #How to add a statistic for a team
     while True:
         team_name = input("Please select a team whose information you would like to modify.") #Finding the team
         if ("statistics" + str(team_name)) in master_dictionary:
-            break;
+            break
     while True:
         statistic_added = input("""You may add a team number (tm), a name (n), a programming language (pl),
         a width or a length for your robot (w or l), whether or not your robot has a camera vision system (cv),
         and the number of drivetrain motors on your robot (dt). Which one of these options would you like
         to edit? Enter your options as the parenthetical after the option you have selected. """) #Note the shorthands like 'pl' here.
         if statistic_added in statistic_list:
-            break;
+            break
     for statistic_name in statistic_list: #For all possible statistic names
         if statistic_added == statistic_name: #If the statistic selected has been found:
             datapoint = (input("What would you like to set this team's " + statistic_added + "?")) #What data the user wants to add.
@@ -86,7 +86,8 @@ while True:
     while valid_input_5 == False:
         menu_selection = input("""Type 'view' to view, 
             'remove' to remove, 'search' to search the teams, 'add' to add a team, or
-            'statistic' to add a statistic to a team's file. """)
+            'statistic' to add a statistic to a team's file. 
+            Or type 'stop' to end this program. """)
         if menu_selection == 'view':
             valid_input_5 = True
             view_function()
@@ -105,4 +106,9 @@ while True:
                 add_statistic()
             else:
                 print("You don't have any teams! Darn it!")
-                continue;
+                continue
+        elif menu_selection == 'stop':
+            break
+        else:
+            print('Enter a valid input! ')
+            continue
