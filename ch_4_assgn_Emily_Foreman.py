7fafc2040eee601f446622b5c0fc9d3e2c32c6c5
Emily Foreman <emily_foreman@icloud.com>
2019-11-13 20:43:28 -0800

    Fix complications with code
    Use self to fix errors

--- ch_4_assgn_Emily_Foreman.py
+++ ch_4_assgn_Emily_Foreman.py
@@ -1,14 +1,12 @@
 import math
-
 class Two_D_Point:
     def __init__ (self, x, y):
         self.x = x
         self.y = y
-    def distance():
+    def distance(self):
         #dist from origin is py theorm : w/ 0,0
-      #  partone = x - 0
-        part_one_calculation = x - 0
-        part_two_calculation = y - 0
+        part_one_calculation = self.x - 0
+        part_two_calculation = self.y - 0
         calculate = part_one_calculation**2 + part_two_calculation**2
         lastpart = math.sqrt(calculate)
         print("The distance from the origin is: ")
@@ -16,23 +14,25 @@ class Two_D_Point:
         return lastpart
 class ThreeD(Two_D_Point):
     def __init__ (self, x, y,z):
-        self.z = tdz
-    def distance():
-        part_one_calculation =x**2 + y**2 + z**2
+        self.z = z
+        super().__init__(x, y)
+    def distance(self):
+        part_one_calculation =self.x**2 + self.y**2 + self.z**2
         part_two_calculation = math.sqrt(part_one_calculation)
         print("The distance from the origin is: ")
         print(part_two_calculation)
         return part_two_calculation
-
+    #need to have an actual instance than call that thing
 check = False
 while check == False:
     x = input("What is the x cordinate? ")
     y = input("What is the y cordinate? ")
     if x.isdigit() and y.isdigit():
-        x = float(x)
-        y = float(y)
+        x = int(x)
+        y = int(y)
         check = True
-Distance  = int(Two_D_Point.distance())
+point_one = Two_D_Point(x, y)
+Distance  = float(point_one.distance())
 checktwo = False
 while checktwo == False:
     TDx = input("What is the x value of the 3D point? ")
@@ -43,8 +43,8 @@ while checktwo == False:
         y = int(TDy)
         z = int(TDz)
         checktwo = True
-
-ThreeD_Distance = int(ThreeD.distance())
+point_two = ThreeD(x, y, z)
+ThreeD_Distance = float(point_two.distance())
 def comparison(Distance, ThreeD_Distance):
     if Distance > ThreeD_Distance:
         print("The 2D point is farther from the origin")
